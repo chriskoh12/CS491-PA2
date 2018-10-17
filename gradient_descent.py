7856d1a70341ec4a0a54ef956de20db16e568da9
chriskoh12 <38603356+chriskoh12@users.noreply.github.com>
2018-10-17 02:04:41 -0700

    Add files via upload

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,3 +1,18 @@
 import numpy as np
 import math
 
+def df(x):
+    return np.array([2 * x[0]])
+
+def gradient_descent(df, x_init, eta):
+    X = df(x_init)
+    step = 0
+    while (np.linalg.norm(X) > 0.0001):
+        step += 1
+        Y = x_init - (eta * X)
+        x_init = Y
+        X = df(Y)
+    #print("num steps is ", step)
+    return Y
+
+x = gradient_descent(df, np.array([5.0]), 0.1)
